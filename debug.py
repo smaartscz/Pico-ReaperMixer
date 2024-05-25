d9cdc715438ecce105dc257c5caf6911ddb8eaf7
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-05-25 19:56:36 +0200

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,14 +1,35 @@
+import asyncio
 from modules.ReaperAPI import ReaperAPI
 import modules.colors as colors
-reaper = ReaperAPI("http://localhost:8080")
+import time
 
-# Initialize communication
-print("Starting communication with REAPER API...")
+async def api_worker():
+    reaper = ReaperAPI("http://localhost:8080")
     
-# Test transport state
-transport_info = reaper.get_transport()
-print(colors.green +f"Transport info: {transport_info}")
-track_info = reaper.get_track_info(5)
-print(colors.green + f"Track Info: {track_info}")
-reaper.mute(5, -1)
-reaper.mute(5, -1)
+    try:
+        # Initialize communication
+        print(colors.red + "Starting communication with REAPER API..." + colors.reset)
+        
+        # Test transport state
+        transport_info = await reaper.get_transport()
+        print(colors.green + f"Transport info: {transport_info}")
+        
+        track_info = await reaper.get_track_info(5)
+        print(colors.green + f"Track Info: {track_info}")
+        
+        await reaper.mute(5, -1)
+        await reaper.mute(5, -1)
+        
+        num = 0.0
+        while num <= 1.0:
+            print(colors.yellow + f"Current volume is: {num}" + colors.reset)
+            await reaper.set_volume(5,num)
+            num += 0.001
+            time.sleep(0.1)
+    finally:
+        await reaper.close()
+
+if __name__ == "__main__":
+    print(colors.red + "Main application is running..." + colors.reset)
+    asyncio.run(api_worker())
+    print(colors.red + "API worker has completed."+ colors.reset)
