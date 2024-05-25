48cae394b02a49aa9a23229f5443dfe349020dd4
smaartscz <65630155+smaartscz@users.noreply.github.com>
2024-05-25 19:56:28 +0200

    Reaper API is now async!
    This means faster response! :)

--- modules/ReaperAPI.py
+++ modules/ReaperAPI.py
@@ -1,32 +1,42 @@
-import requests
+import aiohttp
+import asyncio
 import modules.colors as colors
+
 class ReaperAPI:
     def __init__(self, base_url):
         self.base_url = base_url
+        self.session = aiohttp.ClientSession()
 
-    def _send_request(self, command):
+    async def _send_request(self, command):
         url = f"{self.base_url}/_/{command}"
         print(colors.yellow + f"Sending request to URL: {url}")
         try:
-            response = requests.get(url)
-            response.raise_for_status()
-            print(colors.green + f"Response received: {response.text}")
-            return response.text
-        except requests.RequestException as e:
+            async with self.session.get(url) as response:
+                response.raise_for_status()
+                text = await response.text()
+                print(colors.green + f"Response received: {text}")
+                return text
+        except aiohttp.ClientError as e:
             print(f"Request to {url} failed: {e}")
             return None
 
-    def get_transport(self):
-        return self._send_request('TRANSPORT')
+    async def get_transport(self):
+        return await self._send_request('TRANSPORT')
 
-    def get_track_info(self, index):
-        return self._send_request(f'TRACK/{index}')
+    async def get_track_info(self, index):
+        return await self._send_request(f'TRACK/{index}')
 
-    def mute(self, index, value):
-        return self._send_request(f'SET/TRACK/{index}/MUTE/{value}')
+    async def mute(self, index, value):
+        return await self._send_request(f'SET/TRACK/{index}/MUTE/{value}')
 
-    def play(self):
-        return self._send_request('SET/TRANSPORT/PLAY')
+    async def play(self):
+        return await self._send_request('SET/TRANSPORT/PLAY')
+    
+    async def set_volume(self, index, value):
+        return await self._send_request(f'SET/TRACK/{index}/VOL/{value}')
+    
+    async def stop(self):
+        return await self._send_request('SET/TRANSPORT/STOP')
 
-    def stop(self):
-        return self._send_request('SET/TRANSPORT/STOP')
+    async def close(self):
+        await self.session.close()
